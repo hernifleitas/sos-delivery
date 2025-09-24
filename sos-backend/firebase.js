4d16ecdc8814c46b1f801fc6040606c5f2ced65e
Hernán <98045423+hernifleitas@users.noreply.github.com>
2025-09-24 14:10:57 -0300

    Update firebase.js

--- sos-backend/firebase.js
+++ sos-backend/firebase.js
@@ -1,8 +1,1 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service-account.json"); // poné la ruta correcta
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
